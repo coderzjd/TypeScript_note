1270856bd98e929d9f352599b3fce260e4a74e7b
jdzhang <1633107710@qq.com>
2023-02-22 21:58:39 +0800

    2222

--- src/03.TS类的面向对象/04.类的成员修饰符.ts
+++ src/03.TS类的面向对象/04.类的成员修饰符.ts
@@ -3,7 +3,7 @@
 // 2.private ：私有的属性或者方法，仅在自身类中可见，new的实例及子类中无法访问。
 // 3.protected :受保护的属性或者方法。修饰的仅在类自身及子类中可见，
 class Person {
-  name: string
+  protected name: string
   constructor(name: string) {
     this.name = name
   }
@@ -15,7 +15,7 @@ class Person {
   }
 }
 class Student extends Person {
-  name: string
+  name!: string
   Sno: number
   constructor(name: string, Sno: number) {
     super(name)
--- src/03.TS类的面向对象/06.readonly只读属性.ts
+++ src/03.TS类的面向对象/06.readonly只读属性.ts
@@ -14,5 +14,6 @@ class Person {
 const p1 = new Person('aa', 11, new Person('bb', 11))
 // readonly和const很像，修饰的数据本身无法被修改，但是修饰的是对象时可以被修改
 // p1.name = 11
+// p1.friend= {}
 p1.friend.age = 11
 export {}
