5a32b2377640be52b8d4011d58e22e72ee0d4e6e
coderzjd <1633107710@qq.com>
2023-02-23 20:32:15 +0800

    11111

---
+++ src/08.TS规则解析/01.TS类型检测为鸭子类型.ts
@@ -0,0 +1,17 @@
+// TS类型检测弱类型
+// 如果一个事物看起来像A,闻起来像A,摸起来像A,那么这个事物就是A类型
+class Person {
+  constructor(public name: string, public age: number) {}
+}
+class Dog {
+  constructor(public name: string, public age: number) {}
+}
+function foo(p: Person) {
+  console.log(p.name)
+  console.log(p.age)
+}
+foo(new Person('张三', 18))
+foo(new Dog('点点', 2))
+foo({ name: '王五', age: 20 })
+
+export {}
