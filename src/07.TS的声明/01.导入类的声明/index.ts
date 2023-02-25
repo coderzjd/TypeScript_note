9456c227e7c98c4c4dab5008ab046f5225f0ee72
coderzjd <1633107710@qq.com>
2023-02-25 14:17:31 +0800

    11

--- src/07.TS的声明/01.导入类的声明/index.ts
+++ src/07.TS的声明/01.导入类的声明/index.ts
@@ -1,8 +1,20 @@
 // 1.使用type关键字声明类型
 // 告诉webpack及编译工具解析规则
 import { type IName, getName } from './name'
+import type { IPerson, IPerson2 } from './person'
 const name: IName = {
   firstName: '张',
   lastName: '三',
 }
 console.log(getName(name))
+
+const p1: IPerson = {
+  name: '李四',
+  age: 18,
+  firends: ['张三', '王五'],
+}
+const p2: IPerson2 = {
+  name: '王五',
+  age: 18,
+  firends: [1, 2, 3],
+}
---
+++ src/07.TS的声明/test/person.ts
@@ -0,0 +1,11 @@
+export interface IPerson {
+  name: string
+  age: number
+  firends: string[]
+}
+
+export interface IPerson2 {
+  name: string
+  age: number
+  firends: any[]
+}
